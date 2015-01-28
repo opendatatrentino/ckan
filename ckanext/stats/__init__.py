aafce148b475ecb732736a44e41f09f85fc94b1a
Marco Combetto <marcomb@hotmail.com>
2015-01-28 17:52:42 +0100

    changed position of __init__.py

--- ckanext/stats/__init__.py
+++ ckanext/stats/__init__.py
@@ -1,1 +1,7 @@
-# empty file needed for pylons to find templates in this directory
+# this is a namespace package
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
